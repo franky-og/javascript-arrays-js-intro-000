d01ceeabb864390fae63c280133647cb1f39237d
Franky Liu <cuifranky95@gmail.com>
2019-07-24 00:34:04 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArray () {
-  chocolateBars.unshift("Milky Way")
+function addElementToBeginningOfArray (IceCream, Magnum) {
+  IceCream.unshift(Magnum)
   
 }
 function destructivelyAddElementToBeginningOfArray () {
